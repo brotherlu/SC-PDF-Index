fdfa2c5a9f319fd1ca74046d52969964c4d64e02
Louay Cheikh <brotherlu@gmail.com>
2012-01-27 01:28:10 -0500

    Working Database Generation

--- controllers/reindex.php
+++ controllers/reindex.php
@@ -18,49 +18,112 @@
  * 
  */
 
+// Function to make sure we dont count any blank pages
+	
+function GetNumberOfPages($xml){
+	$i = 0;
+	while ($xml->body->doc->page[$i]->word[0] != ""){
+		$i++;
+		}
+	return $i;
+	}
+
+function CleanWordOfNonLettres($word){
+	
+	$charactersToDrop=array('(',')','/','\\',':',',','.','0','1','2','3','4','5','6','7','8','9');
+	$word=str_replace($charactersToDrop, '', $word);
+	return $word;
+	
+	}
+
+// Primary reindex function
+
 function reindex(){
+
+	// Open the Folder
 
 	if(is_dir(PDF_SOURCE)){
 		if($dh=opendir(PDF_SOURCE)){
 			while (($file = readdir($dh)) !== false) {
-				$fileList[]=$file;
+				$fileList[] = $file;
 			}
 			closedir($dh);
-			}
-		}
+			} else {die("Can not Open Directory, maybe Permissions");};
+		} else {die("The Folder does not Exist");}
 	
-	
-		// Check if there are files to be indexed
-		
-		if($fileList && $fileList[2]){
-	
-/*	for($i=2;$i<count($fileList);$i++){ */
-		
-			$content = shell_exec('pdftotext -bbox '.PDF_SOURCE.$fileList[2].' /tmp/pdftotext.tmp');
-			
-			// capture XML data from PDF
-			$content = shell_exec('cat /tmp/pdftotext.tmp');
-			
-			$xml = simplexml_load_string($content);
-		
-			$TotalPageCount=count($xml->body->doc->page);
-			
-			for($i=0;$i<count($xml->body->doc->page);$i++){
-				
-				echo $xml->body->doc->page[5]->word[0]->attributes()->xMin;
-			
-			}
-		/* } */  
-		} else {echo "There are no files to be indexed in this folder! Please Try Again!";}
+	//	Create the Index Object
 
-/*
 	$index=new Index();
-	
+			
 	$index->ClearAllTables();
-	
+			
 	$index->GenerateInitialDatabaseTables();
+			
+	// Check if there are files to be indexed
+		
+	if($fileList && $fileList[2]){
 	
-*/
+	for($i=2;$i<count($fileList);$i++){ 
+		
+		// Capture XML data from PDF using poppler and cat, stdout did not work
+		// outputs to a /tmp/ file
+		
+		$content = shell_exec('pdftotext -bbox '.PDF_SOURCE.$fileList[$i].' /tmp/pdftotext.tmp');
+			
+		$content = shell_exec('cat /tmp/pdftotext.tmp');
+			
+		$xml = simplexml_load_string($content);
+	
+		$docTitle = $xml->head->title;
+		$docAuthour = $xml->head->meta[0]->attributes()->content;
+		$totalPageCount = GetNumberOfPages($xml);
+			
+		// Start Generating new Data
+			
+		$docId=$index->AddDocList($fileList[$i],$docTitle,$docAuthour,$totalPageCount);
+		
+		if(isset($docId)){
+		
+			// Create Word Table for word parsing
+			
+			if($index->CreateWordTable($docId)){$index->CreateWordTable($docId);} else {die("Could not create Word table!");}
+		
+			for($j = 0;$j < $totalPageCount;$j++){
+				
+				// Get Page Information
+				
+				$page_width = $xml->body->doc->page[$j]->attributes()->width;
+				$page_height = $xml->body->doc->page[$j]->attributes()->height;
+				
+				$index->AddPageList($docId,$j+1,$page_width,$page_height);
+				
+				// Word Time
+				
+				$wordCount = count($xml->body->doc->page[$j]->word);
+				
+				for ($k = 0;$k < $wordCount;$k++){
+					
+					$word = $xml->body->doc->page[$j]->word[$k];
+					
+					$pattern = '([a-zA-Z]+)';
+					
+					if(preg_match($pattern,$word)){
+					
+						$word = CleanWordOfNonLettres($word);
+					
+						$xMin = $xml->body->doc->page[$j]->word[$k]->attributes()->xMin;
+						$xMax = $xml->body->doc->page[$j]->word[$k]->attributes()->xMax;
+						$yMin = $xml->body->doc->page[$j]->word[$k]->attributes()->yMin;
+						$yMax = $xml->body->doc->page[$j]->word[$k]->attributes()->yMax;
+					
+						$index->AddWord($docId,$word,$j+1,$xMin,$xMax,$yMin,$yMax);
+					}
+				}	
+			}
+		} else {die("DOC_LIST Table Creation failed!");}
+		} 
+	} else {echo "There are no files to be indexed in this folder! Please Try Again!";}
+
 
 };
 
--- models/Index.model.php
+++ models/Index.model.php
@@ -45,25 +45,35 @@ class Index extends Universe{
 		
 		$connectDB=parent::connectDB();
 		
-		$addDocListQuery=$connectDB->query("INSERT INTO DOC_LIST SET	doc_filename=$filename,
-																		doc_title=$title,
-																		doc_authour=$authour,
-																		doc_total_pages=$pageTotal ");
+		$addDocListQuery=$connectDB->query("INSERT INTO DOC_LIST SET	doc_filename='$filename',
+																		doc_title='$title',
+																		doc_authour='$authour',
+																		doc_total_pages='$pageTotal' ");
 		
 		if ($addDocListQuery){
 			
-			--------------------------------------------------
+			$findDocListId=$connectDB->query("SELECT doc_id FROM DOC_LIST WHERE doc_filename='$filename' AND doc_title='$title' ");
+			
+			$doc_id=$findDocListId->fetch_array();
+			
+			return $doc_id[0];
+			
 			}
 		
+		return $addDocListQuery;
 		
 		}
 	
 	// Create the Word Tabel for the page
-	function CreateWordTable($page_id){
+	function CreateWordTable($doc_id){
 		
 		$connectDB=parent::connectDB();
 		
-		$createWordTableQuery=$connectDB->query("CREATE TABLE $page_id (word_id int(11) AUTO_INCREMENT, word varchar(256), xMin int(11), xMax int(11), yMin int(11), yMax int(11), PRIMARY KEY (word_id)) ");
+		$doc_id="DOC_".$doc_id;
+		
+		$createWordTableQuery=$connectDB->query("CREATE TABLE $doc_id (word_id int(11) AUTO_INCREMENT, word varchar(256), page_no int(11), xMin int(11), xMax int(11), yMin int(11), yMax int(11), PRIMARY KEY (word_id)) ");
+		
+		return $createWordTableQuery;
 		
 		}
 	
@@ -73,13 +83,15 @@ class Index extends Universe{
 	
 		$connectDB=parent::connectDB();
 		
+		$dropTableQuery=$connectDB->query("DROP TABLE PAGE_LIST");
+		$dropTableQuery=$connectDB->query("DROP TABLE DOC_LIST");
+		
 		$clearAllTablesQuery=$connectDB->query("SHOW TABLES");
 		
 		if ($clearAllTablesQuery){
 			
 			while($row=$clearAllTablesQuery->fetch_array()){
 				
-				print_r($row);
 				$dropTableQuery=$connectDB->query("DROP TABLE $row[0]");
 				
 				}	
@@ -90,4 +102,37 @@ class Index extends Universe{
 			} else {return 0;}
 		
 		}
+	
+	// Adds a page to the PAGE_LIST table
+	
+	function AddPageList($doc_id,$page_no,$page_width,$page_height){
+		
+		$connectDB = parent::connectDB();
+		
+		$addPageListQuery = $connectDB->query("INSERT INTO PAGE_LIST SET 	doc_id='$doc_id',
+																			page_no='$page_no',
+																			page_width='$page_width',
+																			page_height='$page_height'");
+		
+		return $addPageListQuery;
+		
+		}
+	
+	function AddWord($doc_id,$word,$page_no,$xMin,$xMax,$yMin,$yMax){
+		
+		$connectDB=parent::connectDB();
+		
+		$doc_id="DOC_".$doc_id;
+		
+		$addWordQuery=$connectDB->query("INSERT INTO $doc_id SET 	word='$word',
+													page_no='$page_no',
+													xMin='$xMin',
+													xMax='$xMax',
+													yMin='$yMin',
+													yMax='$yMax'");
+		
+		return $addWordQuery;
+		
+		}
+	
 	};
