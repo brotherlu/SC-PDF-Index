123738aa2228c76394549560308e6c9414b10cea
Louay Cheikh <brotherlu@gmail.com>
2012-02-22 17:20:15 -0500

    Added query time for testing

--- views/footer.inc.php
+++ views/footer.inc.php
@@ -5,23 +5,28 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
+ *
  */
 
 -->
 </div> <!-- Main Div ending tag -->
 <div id="footer">
-&copy;<?php echo date('Y'); ?> <b>PHPoppler</b> <?php echo REV; ?> <i>GPLv2</i>
+<?php
+
+$querytime = number_format(microtime(true)-START_TIME,3);
+
+?>
+&copy;<?php echo date('Y'); ?> <b>PHPoppler</b> <?php echo REV; ?> <i>GPLv2</i> <?php echo "<span class=quetime>Query Time: $querytime s</span>"; ?>
 </div>
 
 </div><!-- Wrapper End Tag -->
